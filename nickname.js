aa5fa0c0bb894b1987e2901d16b138ae4eb82070
Tushar Shekhar <tusharshekhar@Tushars-MacBook-Air.local>
2023-11-24 15:39:19 +0530

    hi

--- nickname.js
+++ nickname.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const playerNameInput = document.getElementById("playerName");
-    const nicknameInput = document.getElementById("nickname");
+    const playerNameInput1 = document.getElementById("playerName1");
+    const nicknameInput1 = document.getElementById("nickname1");
+    const playerNameInput2 = document.getElementById("playerName2");
+    const nicknameInput2 = document.getElementById("nickname2");
     const startButton = document.getElementById("startit");
     const nicknameForm = document.getElementById("nicknameForm");
 
@@ -10,8 +12,10 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     // Add event listeners for input changes
-    playerNameInput.addEventListener("input", updateStartButton);
-    nicknameInput.addEventListener("input", updateStartButton);
+    playerNameInput1.addEventListener("input", updateStartButton);
+    nicknameInput1.addEventListener("input", updateStartButton);
+    playerNameInput2.addEventListener("input", updateStartButton);
+    nicknameInput2.addEventListener("input", updateStartButton);
 
     // Add submit event listener to prevent the form submission if inputs are not valid
     nicknameForm.addEventListener("submit", function (event) {
